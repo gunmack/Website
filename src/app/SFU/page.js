3035e40fe12e2837e4c79dde1684c096a9cb628c
Julkar <91394056+gunmack@users.noreply.github.com>
2024-05-10 03:39:25 -0700

    Update page.js

--- src/app/SFU/page.js
+++ src/app/SFU/page.js
@@ -34,23 +34,23 @@ export default function SFU(){
       <h1 className="text-red-500 flex flex-lf justify-center font-bold text-xl"> Campuses </h1>
         <article className=" px-20 py-10 rounded-lg flex flex-lf justify-center">
           <h1>
-            <Link href="https://www.sfu.ca/~jnr2/SFU1/Burnaby.html" target="_blank" className="bg-red-900 hover:bg-red-400 text-white font-bold px-5 py-2 rounded-full"> Burnaby Campus</Link>
+            <Link href="https://www.sfu.ca/~jnr2/SFU1/Burnaby.html" className="bg-red-900 hover:bg-red-400 text-white font-bold px-5 py-2 rounded-full"> Burnaby Campus</Link>
           </h1>
           <h1> 
-            <Link href="https://www.sfu.ca/~jnr2/SFU1/Vancouver.html" target="_blank" className="bg-red-900 hover:bg-red-400 text-white font-bold px-5 py-2 rounded-full"> Vancouver Campus</Link>
+            <Link href="https://www.sfu.ca/~jnr2/SFU1/Vancouver.html"  className="bg-red-900 hover:bg-red-400 text-white font-bold px-5 py-2 rounded-full"> Vancouver Campus</Link>
           </h1>
           <h1> 
             {/* <Link href="/SFU/Surrey" target="_blank" className="bg-red-900 hover:bg-red-400 text-white font-bold py-2 px-10 rounded-full"> Surrey Campus</Link> */}
-            <Link href="https://www.sfu.ca/~jnr2/SFU1/Surrey.html" target="_blank" className="bg-red-900 hover:bg-red-400 text-white font-bold py-2 px-5 rounded-full"> Surrey Campus</Link>
+            <Link href="https://www.sfu.ca/~jnr2/SFU1/Surrey.html"  className="bg-red-900 hover:bg-red-400 text-white font-bold py-2 px-5 rounded-full"> Surrey Campus</Link>
           </h1>
         </article>
         <h1 className="text-red-500 flex flex-lf justify-center font-bold text-xl"> Residences </h1>
         <article className=" px-2 py-10 rounded-lg flex flex-lf justify-center">
           <h1>
-            <Link href="https://www.sfu.ca/~jnr2/SFU1/BNResidence.html" target="_blank" className="bg-red-900 hover:bg-red-400 text-white font-bold py-2 px-2 rounded-full"> Burnaby Residence</Link>
+            <Link href="https://www.sfu.ca/~jnr2/SFU1/BNResidence.html"  className="bg-red-900 hover:bg-red-400 text-white font-bold py-2 px-2 rounded-full"> Burnaby Residence</Link>
           </h1>
           <h1> 
-            <Link href="https://www.sfu.ca/~jnr2/SFU1/VNResidence.html" target="_blank" className="bg-red-900 hover:bg-red-400 text-white font-bold py-2 px-2 rounded-full"> Vancouver Residence</Link>
+            <Link href="https://www.sfu.ca/~jnr2/SFU1/VNResidence.html" className="bg-red-900 hover:bg-red-400 text-white font-bold py-2 px-2 rounded-full"> Vancouver Residence</Link>
           </h1>
         </article><br></br><br></br>
         <footer>
