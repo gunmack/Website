59b681d611367855d0b995c938db774d2d14f22c
gunmack <jreedoy2002@yahoo.com>
2024-10-23 02:45:59 -0700

    updated README.md and added uncommitted changes

--- src/app/page.js
+++ src/app/page.js
@@ -16,26 +16,26 @@ export default function About() {
       <div>
         <article className="font-mono text-neutral-50 bg-cyan-900 rounded-lg flex flex-col items-center text-start text-md">
           <div className="bg-red-900 rounded-lg p-10  m-10 ">
-          <p>
-            Hi, my name is Julkar and welcome to my page :)<br></br> 
-            I am a 2nd year Computing Science Student at Simon Fraser University. <br></br>
-            My preferred languages are:<br></br><br></br> 
-          </p>
+            <p>
+              Hi, my name is Julkar and welcome to my page :)<br></br> 
+              I am a 2nd year Computing Science Student at Simon Fraser University. <br></br>
+              My preferred languages are:<br></br><br></br> 
+            </p>
     
-          <ul>
-            <li>👉 Python</li>
-            <li>👉 Java</li>
-            <li>👉 JavaScript</li>
-            <li>👉 C</li>
-          </ul>
-          <br></br>
+            <ul>
+              <li>👉 Python</li>
+              <li>👉 Java</li>
+              <li>👉 JavaScript</li>
+              <li>👉 C</li>
+            </ul>
+            <br></br>
     
-          <p>   
-            I mostly work on front-end applications with React and Python.<br></br>
-            I have experience working with abstract data structures in C and C++.<br></br>
-            Currently, I am learning Object Oriented Programming with Java.
-            <br></br><br></br>Take a look at my resume <Link href="/files/Julkar_Reedoy_resume.pdf" target="_blank">📄</Link>
-          </p>
+            <p>   
+              I mostly work on front-end applications with React and Python.<br></br>
+              I have experience working with abstract data structures in C and C++.<br></br>
+              Currently, I am learning Object Oriented Programming with Java.
+              <br></br><br></br>Take a look at my resume <Link href="/files/Julkar_Reedoy_resume.pdf" target="_blank">📄</Link>
+            </p>
           </div>
 
           <div>
